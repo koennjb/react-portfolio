f49192850adc70c85e02874ae5aa4e9505d45dc0
Koenn Becker <koenn@uw.edu>
2020-04-11 10:07:41 -0700

    Enabled ESLint with Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-import "./App.css";
-import Button from "./components/Button";
+import React from 'react';
+import './App.css';
+import Button from './components/Button';
 
-function App() {
-  return (
-    <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
-      <h1>Super cool page</h1>
-      <Button onClick={() => console.log("I was clicked")}>
-        I am a button
-      </Button>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
+            <h1>Super cool page</h1>
+            <Button onClick={(): void => console.log('I was clicked')}>I am a button</Button>
+        </div>
+    );
+};
 
 export default App;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,19 @@
-// src/components/button.js
-import React from "react";
-
+import React from 'react';
 interface IProps extends React.HTMLAttributes<HTMLButtonElement> {
-  children: any;
+    children?: React.ReactNode[];
 }
 
-export default function Button(props: IProps) {
-  // If you don't spread children and title, the restProps
-  // still contain these props and compiler will say "no"
-  const { children, title, ...buttonProps } = props;
-  return (
-    <button
-      className="px-2 py-1 rounded-lg bg-green-400 text-green-800 text-xl font-light uppercase shadow-md hover:shadow-lg"
-      {...buttonProps}
-    >
-      {children}
-    </button>
-  );
-}
+const Button: React.FC<IProps> = (props: IProps) => {
+    // If you don't spread children and title, the restProps
+    // still contain these props and compiler will say "no"
+    const { children, ...buttonProps } = props;
+    return (
+        <button
+            className="px-2 py-1 rounded-lg bg-green-400 text-green-800 text-xl font-light uppercase shadow-md hover:shadow-lg"
+            {...buttonProps}
+        >
+            {children}
+        </button>
+    );
+};
+export default Button;
