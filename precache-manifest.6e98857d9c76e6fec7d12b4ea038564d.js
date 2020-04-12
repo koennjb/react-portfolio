3e5abf81b35baf1d40a7e5ad695c70925121285b
Koenn Becker <koenn@uw.edu>
2020-04-12 01:35:47 -0700

    Updates

---
+++ precache-manifest.6e98857d9c76e6fec7d12b4ea038564d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e7163bd164cd1807cd19eab2bf24de2",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "71cf1a318d069277671e",
+    "url": "/react-portfolio/static/css/main.838342f6.chunk.css"
+  },
+  {
+    "revision": "b553378cf575282a664f",
+    "url": "/react-portfolio/static/js/2.8c1902ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.8c1902ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71cf1a318d069277671e",
+    "url": "/react-portfolio/static/js/main.bc9af54e.chunk.js"
+  },
+  {
+    "revision": "a229530a0c0d1ed130b6",
+    "url": "/react-portfolio/static/js/runtime-main.65d8297c.js"
+  }
+]);
