bbd202ecf184a565c3eea57398dc044d8f8deecc
Koenn Becker <koenn@uw.edu>
2020-04-12 01:39:13 -0700

    Added deployment to Github Pages

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,20 @@
-console.log(process.env.NODE_ENV);
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  // Specify the paths to all of the template files in your project
-  content: [
-    "./src/*.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
-  ],
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    // Specify the paths to all of the template files in your project
+    content: [
+        './src/*.{js,jsx,ts,tsx}',
+        './src/**/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,jsx,ts,tsx}',
+        './public/index.html',
+    ],
 
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
 });
 
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    process.env.NODE_ENV === "production" ? purgecss : undefined,
-  ],
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        process.env.NODE_ENV === 'production' ? purgecss : undefined,
+    ],
 };
